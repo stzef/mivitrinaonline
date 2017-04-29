170544a89d323cf0a6ff6e806915c29248c3d03c
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-04-29 08:39:08 -0500

    Commit de apoyo

--- mivitrinaonline/settings.py
+++ mivitrinaonline/settings.py
@@ -36,9 +36,9 @@ THIRTY_PARTY_APPS = (
 #http://azure_account_name.blob.core.windows.net/
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME") if os.environ.get("AZURE_ACCOUNT_NAME") else CREDENTIALS["AZURE_ACCOUNT_NAME"]
-AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY") if os.environ.get("AZURE_ACCOUNT_KEY") else CREDENTIALS["AZURE_ACCOUNT_KEY"]
-AZURE_CONTAINER = os.environ.get("AZURE_CONTAINER") if os.environ.get("AZURE_CONTAINER") else CREDENTIALS["AZURE_CONTAINER"]
+AZURE_ACCOUNT_NAME = os.environ.get("AZURE_ACCOUNT_NAME") if os.environ.get("AZURE_ACCOUNT_NAME",False) else CREDENTIALS["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY") if os.environ.get("AZURE_ACCOUNT_KEY",False) else CREDENTIALS["AZURE_ACCOUNT_KEY"]
+AZURE_CONTAINER = os.environ.get("AZURE_CONTAINER") if os.environ.get("AZURE_CONTAINER",False) else CREDENTIALS["AZURE_CONTAINER"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRTY_PARTY_APPS
 
