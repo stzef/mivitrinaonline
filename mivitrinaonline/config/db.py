931bd4b09cab599e8b269c0c2c4e624e38afad99
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2017-07-22 14:46:49 -0500

    commit de apoyo

---
+++ mivitrinaonline/config/db.py
@@ -0,0 +1,10 @@
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'db_mvo',
+		'USER': 'b9991527095f67',
+		'PASSWORD': '3043c9a5',
+		'HOST': 'us-cdbr-azure-east-c.cloudapp.net',
+		'PORT': '3306',
+	}
+}
